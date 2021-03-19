da924df86cbf81c14fe016ed30c4f874b48f1c4d
Davy Nimbona <davynimbona@gmail.com>
2021-03-19 01:50:28 +0100

    Déployer le l'application

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ from flask import Flask, request, render_template
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
+@app.route('/')
 def upload():
-    return render_template('js/main.html')
+    return render_template('index.html')
 
 
-if __name__ == 'main':
-    app.run(host="0.0.0.0", threaded=True, port=5000, debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
